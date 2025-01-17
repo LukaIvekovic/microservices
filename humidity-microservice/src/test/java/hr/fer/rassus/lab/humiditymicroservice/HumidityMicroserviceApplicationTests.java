d14e935d2cbb8fa44d5136b9ae69e3ecdbd53c7d
Luka Iveković <luka.ivekovic2@gmail.com>
2025-01-17 11:49:03 +0100

    Added initial project structure

---
+++ humidity-microservice/src/test/java/hr/fer/rassus/lab/humiditymicroservice/HumidityMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.rassus.lab.humiditymicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumidityMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
